82961345b938f59ecab79f192ace588522c5b709
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-19 17:25:44 +0300

    Take token from api and set to header

---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <dialog open>
+      <div>Helü</div> 
+  </dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -3,35 +3,46 @@
   <section v-if="!$store.state.login">
     <form @submit.prevent="userLogin">
       <input type="text" placeholder="username" ref="username">
+      <input type="email" placeholder="email" v-model="email">
       <input type="text" placeholder="password" v-model="password">
       <button></button>
     </form>
   </section>
 
   <welcome-page v-else></welcome-page>
-
+  
 </template>
 
 <script>
 import WelcomePage from './WelcomePage'
+import axios from 'axios'
 
 export default {
   name: 'LoginPage',
 
   components: {
-    WelcomePage
+    WelcomePage,
   },
 
   data() {
     return {
-      password: '',
+      email: '',
+      password: ''
     }
   },
 
   methods: {
-    userLogin() {
-      this.$store.state.login = true
-      this.$store.state.username = this.$refs.username.value
+    async userLogin() {
+
+      const response = await axios.post('https://aodapi.eralpsoftware.net/login/apply', {
+        username: this.email,
+        password: this.password
+      })
+        this.$store.state.login = true
+        this.$store.state.username = this.$refs.username.value
+        localStorage.setItem('token', response.data.token)
+
+        axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token')
     }
   }
 }
