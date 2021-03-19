4e4e6c043e82408e4346d5523df3590509967683
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-19 22:00:19 +0300

    Add crypto.js

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,8 +4,8 @@
     <form @submit.prevent="userLogin">
       <input type="text" placeholder="username" ref="username">
       <input type="email" placeholder="email" v-model="email">
-      <input type="text" placeholder="password" v-model="password">
-      <button></button>
+      <input type="password" placeholder="password" v-model="password">
+      <button>Login</button>
     </form>
   </section>
 
@@ -16,6 +16,7 @@
 <script>
 import WelcomePage from './WelcomePage'
 import axios from 'axios'
+import CryptoJS from "crypto-js";
 
 export default {
   name: 'LoginPage',
@@ -40,7 +41,15 @@ export default {
       })
         this.$store.state.login = true
         this.$store.state.username = this.$refs.username.value
-        localStorage.setItem('token', response.data.token)
+
+        
+        const encToken = CryptoJS.AES.encrypt(response.data.token, 'secret key 123').toString();
+        const decrypToken = CryptoJS.AES.decrypt(encToken, "secret key 123").toString(CryptoJS.enc.Utf8)
+        console.log(encToken)
+        console.log(decrypToken, localStorage.getItem('token'))
+        console.log(response.data.token)
+
+        localStorage.setItem('token', encToken)
 
         axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token')
     }
