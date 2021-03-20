218ea02d250221549c59f415472b92788a5f3a65
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-20 23:56:20 +0300

    Use vuex and handle errors

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>Welcome</h1>
-  <h2>{{ $store.state.username }}</h2>
+  <h2>{{ user.username }}</h2>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     name: 'WelcomePage',
+
+    computed: {
+    ...mapGetters(['user']),
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,43 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import CryptoJS from "crypto-js";
+
 
 export default createStore({
   state: {
     login: false,
-    username: '',
+    user: {},
   },
+
   mutations: {
+    userLogin(state, payload) {
+      state.user = payload
+      // console.log('stateuser',state.user)
+      state.login = true
+    }
   },
+
   actions: {
+    async userLogin(context, payload) {
+      // console.log(context, payload)
+      try {
+          await axios.post('https://aodapi.eralpsoftware.net/login/apply', {
+            username: payload.email,
+            password: payload.password
+          })
+      
+          context.commit('userLogin', payload)
+      }
+      catch(err) {
+          alert(err)
+      }
+    }
+  },
+
+  getters: {
+  user(state) {
+    return state.user
+  }
   },
   modules: {
   }
